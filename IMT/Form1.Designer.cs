7ba128bf58421f82d0367f78b2b3faba3af7c5a5
ZeroPrin <panchenko.glebka@gmail.com@>
2023-03-14 23:00:15 +0300

    Добавьте файлы проекта.

---
+++ IMT/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace IMT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(111, 181);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(111, 215);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(46, 179);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Рост";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(46, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Вес";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(231, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Посчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(135, 355);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 31);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "ИМТ = 0";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.panel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.panel1.Location = new System.Drawing.Point(413, 367);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(72, 19);
+            this.panel1.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.PaleGreen;
+            this.panel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.panel2.Location = new System.Drawing.Point(485, 367);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(72, 19);
+            this.panel2.TabIndex = 10;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.panel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.panel3.Location = new System.Drawing.Point(557, 367);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(72, 19);
+            this.panel3.TabIndex = 11;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.panel4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.panel4.Location = new System.Drawing.Point(629, 367);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(72, 19);
+            this.panel4.TabIndex = 12;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(413, 316);
+            this.trackBar1.Maximum = 12;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(288, 45);
+            this.trackBar1.TabIndex = 13;
+            this.trackBar1.Value = 1;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::IMT.Properties.Resources._2f;
+            this.pictureBox3.Location = new System.Drawing.Point(413, 22);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(288, 288);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 14;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::IMT.Properties.Resources._6637750_boy_male_man_sex_icon__2_;
+            this.pictureBox2.Location = new System.Drawing.Point(188, 22);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(130, 130);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = global::IMT.Properties.Resources._6637778_female_girl_profile_sex_woman_icon__1_;
+            this.pictureBox1.Location = new System.Drawing.Point(37, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(130, 130);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ IMT/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMT
+{
+    public partial class Form1 : Form
+    {
+        bool gender = false;
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox3.Image = Image.FromFile("C:\\Users\\Home\\Documents\\VS 2022\\IMT\\IMT\\Resources\\2f.png");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.BorderStyle != BorderStyle.Fixed3D) 
+            {
+                pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+                pictureBox2.BorderStyle = BorderStyle.None;
+                gender = false;
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox2.BorderStyle != BorderStyle.Fixed3D)
+            {
+                pictureBox2.BorderStyle = BorderStyle.Fixed3D;
+                pictureBox1.BorderStyle = BorderStyle.None;
+                gender = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int height = Convert.ToInt32(textBox1.Text);
+                int weight = Convert.ToInt32(textBox2.Text);
+
+                double imt = weight/Math.Pow(((double)height/100),2);
+                label2.Text = "ИМТ = " + Math.Round(imt,1).ToString();
+
+                if (imt < 18.5)
+                {
+                    trackBar1.Value = 2;
+                    if (gender)
+                        pictureBox3.Image = Image.FromFile("C:\\Users\\Home\\Documents\\VS 2022\\IMT\\IMT\\Resources\\1m.png");
+                    else
+                        pictureBox3.Image = Image.FromFile("C:\\Users\\Home\\Documents\\VS 2022\\IMT\\IMT\\Resources\\1f.png");
+
+                }
+                else if (imt < 25)
+                {
+                    trackBar1.Value = 5;
+                    if (gender)
+                        pictureBox3.Image = Image.FromFile("C:\\Users\\Home\\Documents\\VS 2022\\IMT\\IMT\\Resources\\2m.png");
+                    else
+                        pictureBox3.Image = Image.FromFile("C:\\Users\\Home\\Documents\\VS 2022\\IMT\\IMT\\Resources\\2f.png");
+                }
+                else if (imt < 30)
+                {
+                    trackBar1.Value = 8;
+                    if (gender)
+                        pictureBox3.Image = Image.FromFile("C:\\Users\\Home\\Documents\\VS 2022\\IMT\\IMT\\Resources\\3m.png");
+                    else
+                        pictureBox3.Image = Image.FromFile("C:\\Users\\Home\\Documents\\VS 2022\\IMT\\IMT\\Resources\\3f.png");
+                }
+                else
+                {
+                    trackBar1.Value = 11;
+                    if (gender)
+                        pictureBox3.Image = Image.FromFile("C:\\Users\\Home\\Documents\\VS 2022\\IMT\\IMT\\Resources\\4m.png");
+                    else
+                        pictureBox3.Image = Image.FromFile("C:\\Users\\Home\\Documents\\VS 2022\\IMT\\IMT\\Resources\\4f.png");
+                }
+            }
+            catch 
+            {
+                MessageBox.Show("Введите правильные данные");
+            }
+            
+            
+        }
+    }
+}
